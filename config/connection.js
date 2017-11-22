d52762593bf13bb749a2427727d37086d6819bba
Chryssa <chryssajones@gmail.com>
2017-11-21 17:59:05 -0800

    set up jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // set up mySQL connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "cjones01",
+    database: "burgers_db"
+  });
+};
 
 // Connect to database
 connection.connect(function(err) {
